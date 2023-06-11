035ea0d0084bdcbd24a973c6773f24f51b0b4b62
asartorio <alejandro.sartorio@gmail.com>
2023-06-11 18:48:56 -0300

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+
+    'name': 'wsf_noticias',
+    'version': '1',
+    'summary': 'WSF Noticias',
+    'sequence': -101,
+    'description': """WSF Noticias""",
+    'author': 'MAN Group 2 ',
+    'maintainer': 'MAN Group 2',
+    'website': 'https://www.facebook.com/wsfnet',
+    'license': 'AGPL-3',
+
+    'depends': [],
+
+    'data': [
+
+    ],
+    'demo': [],
+    'qweb': [],
+    'images': ['static/description/icon.png'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
