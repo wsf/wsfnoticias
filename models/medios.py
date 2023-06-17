e6766b831f13dba12420657522ab080fe308d437
asartorio <alejandro.sartorio@gmail.com>
2023-06-17 16:38:33 -0300

    actualizaciones separadas por importancia de medios

--- models/medios.py
+++ models/medios.py
@@ -15,7 +15,7 @@ class Medios(models.Model):
     pagina_web = fields.Char('Pagina Web:')
     pagina_rss = fields.Char('Pagina rss:')
     regla = fields.Many2one('wsf_noticias_reglas')
-    importancia = fields.Selection([('bajo', 'Bajo'), ('medio', 'Medio'), ('alto', 'Alto')])
+    importancia = fields.Selection([('baja', 'Baja'), ('media', 'Media'), ('alta', 'Alta')])
     pauta = fields.Float('Pauta')
     estado = fields.Selection([('on', 'ON'), ('off', 'OFF')], required=True)
     puntuacion = fields.Char('Puntuacion')
@@ -29,127 +29,187 @@ class Medios(models.Model):
             result.append((rec.id, '%s' % (rec.medio.display_name)))
         return result
 
-    @api.model
-    def scrap_noticias(self):
-        all_records = self.env['wsf_noticias_medios'].search([])
+    def scrap_importancia_baja(self):
+        self.scrap_noticias('baja')
+
+    def scrap_importancia_alta(self):
+        self.scrap_noticias('alta')
+
+    def scrap_importancia_media(self):
+        self.scrap_noticias('media')
+
+    def scrap_importancia_todos(self):
+        self.scrap_noticias('todos')
+
+    #@api.model
+    def scrap_noticias(self, importancia="todos"):
+
+
+        filtro_importancia = []
+
+        if importancia == 'baja':
+            filtro_importancia = [('importancia', '=', 'baja')]
+        elif importancia == 'media':
+            filtro_importancia = [('importancia', '=', 'media')]
+        elif importancia == 'alta':
+            filtro_importancia = [('importancia', '=', 'alta')]
+        else:
+            filtro_importancia = []
+
+        all_records = self.env['wsf_noticias_medios'].search(filtro_importancia,order='medio asc')
         all_records_resultados = self.env['wsf_noticias_resultados'].search([])
 
         for rec in all_records:
-            limite = rec.limite
 
-            datos = {}
-            datos['Noticias'] = {}
+            try:
+                limite = rec.limite
 
-            # Llamar al archivo JSON
+                datos = {}
+                datos['Noticias'] = {}
 
-            paginas = {
-                rec.medio.id: {
-                    "rss": rec.pagina_rss,
-                    "link": rec.pagina_web
+                # Llamar al archivo JSON
+
+                paginas = {
+                    rec.medio.id: {
+                        "rss": rec.pagina_rss,
+                        "link": rec.pagina_web
+                    }
                 }
-            }
-            lista_termninos = []
-            for t in rec.regla.terminos_and:
-                lista_termninos.append(t.name)
-            # Iterar por cada pagina de noticias
+                lista_termninos = []
+                for t in rec.regla.terminos_and:
+                    lista_termninos.append(t.name)
+                # Iterar por cada pagina de noticias
 
-            contador = 1
-            if rec.estado == 'on':
-                for pagina, valor in paginas.items():
-                    # Checar si hay un link rss en el json para darle prioridad
-                    if 'rss' in valor and valor['rss'] != False:
-                        v = fp.parse(valor['rss'])
-                        print("Descargando articulos de: ",
-                              pagina)  # Se crea un diccionario con la palabra reservada newspaper jalando los datos de nuestro json
-                        newsPaper = {
-                            "pagina": pagina,
-                            "link": valor['link'],
-                            "articulos": []  # Se crea un elemento en blanco, donde se guardaran las noticias
-                        }
-                        for entrada in v.entries:
+                contador = 1
+                if rec.estado == 'on':
+                    for pagina, valor in paginas.items():
+                        # Checar si hay un link rss en el json para darle prioridad
+                        if 'rss' in valor and valor['rss'] != False:
+                            v = fp.parse(valor['rss'])
+                            print("Descargando articulos de: ",
+                                  pagina)  # Se crea un diccionario con la palabra reservada newspaper jalando los datos de nuestro json
+                            newsPaper = {
+                                "pagina": pagina,
+                                "link": valor['link'],
+                                "articulos": []  # Se crea un elemento en blanco, donde se guardaran las noticias
+                            }
+                            for entrada in v.entries:
 
-                            if contador > limite:
-                                break
-                            article = {}
+                                if contador > limite:
+                                    break
+                                article = {}
 
-                            # article['keywords'] = entrada
-                            try:
-                                contenido = Article(entrada.link)
-                                contenido.download()
-                                contenido.parse()
-                            except Exception as e:
-                                print(e)
-                                continue
+                                # article['keywords'] = entrada
+                                try:
+                                    contenido = Article(entrada.link)
+                                    contenido.download()
+                                    contenido.parse()
+                                except Exception as e:
+                                    print(e)
+                                    continue
 
-                            article['titulo'] = contenido.title
+                                article['titulo'] = contenido.title
 
-                            try:
-                                encontrado = self.env['wsf_noticias_resultados'].search(
-                                    [('titulo', '=', article['titulo'])])
-                                if encontrado:
-                                    pass
-                                else:
-                                    article['medio'] = rec.medio.id
-                                    article['copete'] = contenido.meta_description  ##
-                                    article['texto'] = contenido.text
-                                    article['link'] = contenido.url
-                                    fecha = contenido.publish_date.strftime('%Y/%m/%d %H:%M:%S')
-                                    article['fecha_hora'] = datetime.strptime(fecha, '%Y/%m/%d %H:%M:%S')
+                                try:
+                                    encontrado = self.env['wsf_noticias_resultados'].search(
+                                        [('titulo', '=', article['titulo'])])
+                                    if encontrado:
+                                        pass
+                                    else:
+                                        article['medio'] = rec.medio.id
+                                        article['copete'] = contenido.meta_description  ##
+                                        article['texto'] = contenido.text
+                                        article['link'] = contenido.url
+                                        fecha = contenido.publish_date.strftime('%Y/%m/%d %H:%M:%S')
+                                        article['fecha_hora'] = datetime.strptime(fecha, '%Y/%m/%d %H:%M:%S')
 
-                                    all_records_resultados.create(article)
-                                    contador = contador + 1
+                                        all_records_resultados.create(article)
+                                        contador = contador + 1
 
-                            except Exception as e:
-                                print(e)
+                                except Exception as e:
+                                    print(e)
 
-                    else:
+                        else:
 
-                        hoja = newspaper.build(valor['link'], memoize_articles=False)
-                        newsPaper = {
-                            "medio": pagina,
-                            "link": valor['link'],
-                            "articulos": []
-                        }
+
+                            url_medio = valor['link']
+                            hoja = newspaper.build(url_medio, memoize_articles=False)
+
+                            newsPaper = {
+                                "medio": pagina,
+                                "link": valor['link'],
+                                "articulos": []
+                            }
+                            contador = 1
+
+                            # hoja.articles -> obtiene una lista con todos los artículos del portal que está visitando (escrapeando)
+                            for contenido in hoja.articles:  # recorre cada uno de los artículos
+                                if contador > limite:
+                                    break
+                                try:
+                                    contenido.download()
+                                    contenido.parse()
+                                except Exception as e:
+                                    print(e)
+                                    continue
+
+                                # noticia concreta
+                                article = {}
+                                article['titulo'] = contenido.title
+
+                                if lista_termninos or True:
+                                    # if lista_termninos[0] in contenido.text and lista_termninos[1] in contenido.text:
+                                    # if lista_termninos[0] in contenido.text or True:
+                                    if True:
+                                        try:
+                                            encontrado = self.env['wsf_noticias_resultados'].search(
+                                                [('titulo', '=', article['titulo'])])
+
+                                            print(contenido.text)
+
+                                            if encontrado:
+                                                pass
+                                            else:
+                                                article['medio'] = rec.medio.id
+                                                article['copete'] = contenido.meta_description ##
+                                                article['texto'] = contenido.text
+
+                                                try:
+                                                    article['link'] = contenido.url
+
+                                                except:
+                                                    pass
+
+                                                # se asegura que que el link comience con la url de la página
+                                                # es para que no se vaya a otro dominio
+                                                url_medio2 = url_medio.replace("https","http")
+                                                condi = article['link'][0:len(url_medio2)+1].replace("http://","").replace("https://","") == url_medio.replace("http://","").replace("https://","")
+                                                condi2 = "/rss" in article['link'] or "/feed" in article['link']
+
+                                                if not condi and not condi2:
+                                                    break
+
+                                                try:
+                                                    fecha2 = contenido.publish_date.strftime('%Y/%m/%d %H:%M:%S')
+                                                    article['fecha_hora'] = datetime.strptime(fecha2,
+                                                                                              '%Y/%m/%d %H:%M:%S')
+                                                except:
+                                                    pass
+
+
+                                                article['regla'] = rec.regla.id
+                                                article['titulo'] = contenido.title
+
+                                                print(contenido.title)
+
+                                                all_records_resultados.create(article)
+                                                contador = contador + 1
+
+                                        except Exception as e:
+                                            print(e)
+
                         contador = 1
-
-                        # hoja.articles -> obtiene una lista con todos los artículos del portal que está visitando (escrapeando)
-                        for contenido in hoja.articles:  # recorre cada uno de los artículos
-                            if contador > limite:
-                                break
-                            try:
-                                contenido.download()
-                                contenido.parse()
-                            except Exception as e:
-                                print(e)
-                                continue
-
-                            # noticia concreta
-                            article = {}
-                            article['titulo'] = contenido.title
-
-                            if lista_termninos:
-                                # if lista_termninos[0] in contenido.text and lista_termninos[1] in contenido.text:
-                                if lista_termninos[0] in contenido.text:
-                                    try:
-                                        encontrado = self.env['wsf_noticias_resultados'].search(
-                                            [('titulo', '=', article['titulo'])])
-                                        if encontrado:
-                                            pass
-                                        else:
-                                            article['medio'] = rec.medio.id
-                                            article['copete'] = contenido.meta_description ##
-                                            article['texto'] = contenido.text
-                                            article['link'] = contenido.url
-                                            fecha2 = contenido.publish_date.strftime('%Y/%m/%d %H:%M:%S')
-                                            article['fecha_hora'] = datetime.strptime(fecha2, '%Y/%m/%d %H:%M:%S')
-                                            article['regla'] = rec.regla.id
-
-                                            all_records_resultados.create(article)
-                                            contador = contador + 1
-
-                                    except Exception as e:
-                                        print(e)
-
-                    contador = 1
-            else:
+                else:
+                    pass
+            except:
                 pass
