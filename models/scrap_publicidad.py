523d19e0e1ff78b3a971629d30baa1562d09288f
asartorio <alejandro.sartorio@gmail.com>
2023-09-06 12:04:50 -0300

    probando con selenium headless en el script 3

--- models/scrap_publicidad.py
+++ models/scrap_publicidad.py
@@ -460,7 +460,10 @@ def tomar_literales_url(contenido):
     from selenium import webdriver
 
     options = webdriver.ChromeOptions()
-    options.headless = True
+
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
 
     driver = webdriver.Chrome(options=options)
     url = contenido.source_url
